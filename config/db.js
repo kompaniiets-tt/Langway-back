b9e05e929e24f7cc15c57f169ab6f53f2c456dec
kompaniiets-t <kompaniets.t@gmail.com>
2022-11-01 19:47:17 +0200

    init

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const { MONGO_URI } = process.env;
+
+exports.connect = () => {
+    mongoose
+        .connect('mongodb+srv://kompanietst:Rjnbyzgfkrf123@cluster0.luo11.mongodb.net/test', {})
+        .then(() => {
+            console.log("Successfully connected to database");
+        })
+        .catch((err) => {
+            console.log("database connection failed. exiting now...");
+            console.error(err);
+            process.exit(1);
+        })
+}
